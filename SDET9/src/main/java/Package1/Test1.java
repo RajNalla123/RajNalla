2469e7a80bd78e1b08b83adecd6772e176d959c0
Viyaan Reddy <Viyaan Reddy@LAPTOP-HN4UC1CH>
2022-10-19 11:03:16 +0530

    changed from unstaged to staged area

---
+++ SDET9/src/main/java/Package1/Test1.java
@@ -0,0 +1,14 @@
+package Package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=24;
+		int b=12;
+		
+		System.out.println(a);
+
+	}
+
+}
