d424b7de18c926604366b7a03fc231b6173747f9
RajNalla123 <rajashekarreddy606@gmail.com>
2022-10-19 17:45:26 +0530

    Merge branch 'master' of https://github.com/RajNalla123/RajNalla.git
    raj

--- SDET9/src/test/java/mygit.java
+++ SDET9/src/test/java/mygit.java
@@ -3,7 +3,7 @@ public class mygit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi how r u");
+        System.out.println("hi how r u");
 	}
 
 }
